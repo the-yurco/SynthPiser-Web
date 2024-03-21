28864de0e5f82036ca8bb8f2483fdfc0764244d8
rondindon <ronnie.rybarik@gmail.com>
2024-03-21 10:14:29 +0100

    lol

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var cors_1 = __importDefault(require("cors"));
-var app = (0, express_1.default)();
-var PORT = 5000;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 5001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.listen(PORT, function () {
-    console.log("Server is running on http://localhost:".concat(PORT));
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
